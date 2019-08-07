e7c73053c0c99d24719cb4d5611e65e11e5597c6
mindaugas <mindaugas@zenitech.co.uk>
2019-08-07 07:58:02 +0300

    Add test for 'findWords'

--- test/Spec.hs
+++ test/Spec.hs
@@ -14,8 +14,11 @@ main = hspec $ do
             findWord grid "PERL" `shouldBe` Just "PERL"    
             findWord grid "RUBY" `shouldBe` Just "RUBY"   
             findWord grid "COBOL" `shouldBe` Just "COBOL"     
-
-    describe "findWord" $ do
         it "Should return 'Nothing' for words that does not exist on the Grid" $ do
-            findWord grid "Mindaugas" `shouldBe` Nothing        
+            findWord grid "Mindaugas" `shouldBe` Nothing                
         
+    describe "findWords" $ do
+        it "Should find all words that exist on the Grid" $ do 
+            findWords grid languages `shouldBe` languages   
+        it "Should not find words that do not exist" $ do
+            findWords grid ["Mindaugas", "Robertas", "Jonas"] `shouldBe` [] 
