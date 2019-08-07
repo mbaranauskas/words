2a791b5e727cf2060a4d91ca7c7e7f234dbb0e5c
mindaugas <mindaugas@zenitech.co.uk>
2019-08-07 07:48:20 +0300

    Add real tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,21 @@
 import Test.Hspec
 import Lib
+import Data
 
 main :: IO ()
 main = hspec $ do
-    describe "How to write a test" $ do
-        it "Should be able to run tests" $ do
-            someString `shouldBe` "someFunc"
+    describe "formatGrid" $ do
+        it "Should concatenate every line with a newline" $ do
+            (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi\n"
 
-            
+    describe "findWord" $ do
+        it "Should find words that exist on the Grid" $ do
+            findWord grid "HASKELL" `shouldBe` Just "HASKELL" 
+            findWord grid "PERL" `shouldBe` Just "PERL"    
+            findWord grid "RUBY" `shouldBe` Just "RUBY"   
+            findWord grid "COBOL" `shouldBe` Just "COBOL"     
+
+    describe "findWord" $ do
+        it "Should return 'Nothing' for words that does not exist on the Grid" $ do
+            findWord grid "Mindaugas" `shouldBe` Nothing        
+        
