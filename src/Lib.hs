00bdc1320dfb99e54a8163f9b417a98540b493a0
mindaugas <mindaugas@zenitech.co.uk>
2019-08-07 07:33:07 +0300

    Refactor grid to separate Data module

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,5 @@
 module Lib
-    ( grid
-    , languages
+    ( languages
     , formatGrid
     , outputGrid
     , findWord
@@ -11,6 +10,7 @@ module Lib
 
 import Data.List(isInfixOf, transpose)
 import Data.Maybe(catMaybes)
+import Data
 
 type Grid = [String]
 
@@ -42,29 +42,3 @@ skew :: Grid -> Grid
 skew [] = []
 skew (l:ls) = l : skew (map indent ls)
     where indent line = '_' : line
-
-grid = [ "__C________R___"
-       , "__SI________U__"
-       , "__HASKELL____B_"
-       , "__A__A_____S__Y"
-       , "__R___B___C____"
-       , "__PHP____H_____"
-       , "____S_LREP_____"
-       , "____I__M_Y__L__"
-       , "____L_E__T_O___"
-       , "_________HB____"
-       , "_________O_____"
-       , "________CN_____"
-       ]
-
-languages = [ "BASIC"
-            , "COBOL"
-            , "CSHARP"
-            , "HASKELL"
-            , "LISP"
-            , "PERL"
-            , "PHP"
-            , "PYTHON"
-            , "RUBY"
-            , "SCHEME"
-            ]
